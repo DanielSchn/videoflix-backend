3757256b212e44ee4352bb6ef68445d470d18611
DanielSchn <d.schneider@dsc-mail.de>
2024-11-18 15:29:54 +0100

    add video upload

--- videoflix_app/admin.py
+++ videoflix_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Video
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'created_at']
+    list_filter = ['title', 'created_at']
 
 # Register your models here.
+admin.site.register(Video, VideoAdmin)
--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+
+    video_file = models.FileField(upload_to='videos', blank=True, null=True)
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
