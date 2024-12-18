6e8f0c1f8e3f36a6d1255b579bfd2972329f2639
DanielSchn <d.schneider@dsc-mail.de>
2024-12-18 11:51:56 +0100

    add read_only_fields

--- videoflix_app/api/serializers.py
+++ videoflix_app/api/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from videoflix_app.models import Video
 
 class VideoSerializer(serializers.ModelSerializer):
+    original_file = serializers.FileField(write_only=True)
+
     class Meta:
         model = Video
-        fields = ['id', 'title', 'description', 'created_at', 'video_480p', 'video_720p', 'video_1080p', 'original_file', 'thumbnail', 'category']
+        fields = ['id', 'title', 'description', 'created_at', 'video_480p', 'video_720p', 'video_1080p', 'thumbnail', 'category', 'original_file']
+        read_only_fields = ['video_480p', 'video_720p', 'video_1080p', 'category']
--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -7,7 +7,7 @@ class Video(models.Model):
     video_480p = models.FileField(upload_to='videos/480p/', blank=True, null=True)
     video_720p = models.FileField(upload_to='videos/720p/', blank=True, null=True)
     video_1080p = models.FileField(upload_to='videos/1080p/', blank=True, null=True)
-    thumbnail = models.FileField(upload_to='img/', blank=True, null=True)
+    thumbnail = models.FileField(upload_to='img/', blank=True, null=True, help_text='Bitte Datei im Format beliebigerName_erlaubteKategorie.jpg oder .png! Erlaubte Kategorien: sports, documentary, romance, crime')
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
